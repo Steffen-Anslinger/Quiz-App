3e01308e674856284b9f6301a9e64508c866f0ef
Steffen Anslinger <steffen.anslinger@hotmail.com>
2023-11-16 15:52:53 +0100

    Updated javascript for button functionality, updated index.html

--- javascript.js
+++ javascript.js
@@ -1,5 +1,20 @@
-const button = document.querySelector('[data-js="button"]');
+const buttonQuestion1 = document.querySelector('[data-js="answer-button"]');
+const buttonQuestion2 = document.querySelector('[data-js="answer-button2"]');
+const buttonQuestion3 = document.querySelector('[data-js="answer-button3"]');
+// const button = document.querySelectorAll;
 
-button.addEventListener("click", () => {
-  console.log("Yeah");
+// button.addEventListener("click", () => {
+//   console.log("Yeah");
+// });
+
+buttonQuestion1.addEventListener("click", () => {
+  document.querySelector(".Answer1").style.display = "block";
+});
+
+buttonQuestion2.addEventListener("click", () => {
+  document.querySelector(".Answer2").style.display = "block";
+});
+
+buttonQuestion3.addEventListener("click", () => {
+  document.querySelector(".Answer3").style.display = "block";
 });
