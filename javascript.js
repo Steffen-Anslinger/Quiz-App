ac6c600cf5c873ba91e09f61cbd929c5759c13eb
Steffen Anslinger <steffen.anslinger@hotmail.com>
2023-11-16 16:53:37 +0100

    Update bookmark buttons, picture-change

--- javascript.js
+++ javascript.js
@@ -18,3 +18,19 @@ buttonQuestion2.addEventListener("click", () => {
 buttonQuestion3.addEventListener("click", () => {
   document.querySelector(".Answer3").style.display = "block";
 });
+
+const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+const bookmarkButton2 = document.querySelector('[data-js="bookmark2"]');
+const bookmarkButton3 = document.querySelector('[data-js="bookmark3"]');
+
+bookmarkButton.addEventListener("click", () =>
+  bookmarkButton.classList.add("change_bookmark")
+);
+
+bookmarkButton.addEventListener("click", () =>
+  bookmarkButton2.classList.add("change_bookmark2")
+);
+
+bookmarkButton.addEventListener("click", () =>
+  bookmarkButton3.classList.add("change_bookmark3")
+);
